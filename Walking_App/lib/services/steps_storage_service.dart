18edfa6c87b2c52a041586f3915f242cf9125f76
NTS <slweetlov4@gmail.com>
2025-04-09 18:05:21 +0700

    Lỗi từ commit trước đó: không nhận uid Fixed

--- Walking_App/lib/services/steps_storage_service.dart
+++ Walking_App/lib/services/steps_storage_service.dart
@@ -1,28 +1,28 @@
-import 'package:hive/hive.dart';
-import 'package:intl/intl.dart';
+// import 'package:hive/hive.dart';
+// import 'package:intl/intl.dart';
 
-class StepStorageService {
-  static final Box<int> _box = Hive.box<int>('steps');
+// class StepStorageService {
+//   static final Box<int> _box = Hive.box<int>('steps');
 
-  static String _getTodayKey() {
-    return DateFormat('yyyy-MM-dd').format(DateTime.now());
-  }
+//   static String _getTodayKey() {
+//     return DateFormat('yyyy-MM-dd').format(DateTime.now());
+//   }
 
-  static int getTodaySteps() {
-    return _box.get(_getTodayKey(), defaultValue: 0) ?? 0;
-  }
+//   static int getTodaySteps() {
+//     return _box.get(_getTodayKey(), defaultValue: 0) ?? 0;
+//   }
 
-  static void saveTodaySteps(int steps) {
-    _box.put(_getTodayKey(), steps);
-  }
+//   static void saveTodaySteps(int steps) {
+//     _box.put(_getTodayKey(), steps);
+//   }
 
-  static void resetIfNewDay() {
-    final today = _getTodayKey();
-    final keys = _box.keys.cast<String>();
-    for (final key in keys) {
-      if (key != today) {
-        _box.delete(key); // Xoá ngày cũ (tuỳ bạn có thể giữ lại nếu cần history)
-      }
-    }
-  }
-}
+//   static void resetIfNewDay() {
+//     final today = _getTodayKey();
+//     final keys = _box.keys.cast<String>();
+//     for (final key in keys) {
+//       if (key != today) {
+//         _box.delete(key); // Xoá ngày cũ (tuỳ bạn có thể giữ lại nếu cần history)
+//       }
+//     }
+//   }
+// }
